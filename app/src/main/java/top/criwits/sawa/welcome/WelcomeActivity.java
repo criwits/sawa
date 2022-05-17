444a5248e64c75c6ed5b6be033febefb8a0a7314
criwits <hanswan@tom.com>
2022-05-17 11:00:32 +0800

    Welcome activity finished

---
+++ app/src/main/java/top/criwits/sawa/welcome/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package top.criwits.sawa.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import top.criwits.sawa.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
